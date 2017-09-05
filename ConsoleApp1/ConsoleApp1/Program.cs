268e8b611f557703ecad8d8e30352d363766994e
tomekgib <tomekgib@pro.onet.pl>
2017-09-05 13:11:12 +0200

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
+            //Code was added in GitHub
             Console.WriteLine("Testing GitHub");
         }
     }
